0248d039eaa8a784d9254c5fd116e2ebd3e39808
Axel Hugo <axelhugoo@MacBook-Pro.local>
2022-03-04 21:14:09 +0700

    Create push bullet integration

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const { default: axios } = require("axios");
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 8080;
+const THRESHOLD_MIN = 30;
+const THRESHOLD_MAX = 70;
 
 const URL =
   "https://www.alphavantage.co/query?function=RSI&symbol=EURUSD&interval=60min&time_period=14&series_type=open&apikey=L1JJOTO2HZTSENMI";
@@ -16,13 +18,58 @@ const getRSIFromAPI = async () => {
   }
 };
 
+const postRSIToPB = async (data) => {
+  try {
+    const url = "https://api.pushbullet.com/v2/pushes";
+    const payload = {
+      device_iden: "ujDfGNf4lk4sjys80jvS1s",
+      type: "note",
+      title: "FX Notif - RSI",
+      body: JSON.stringify(data),
+    };
+    const headers = {
+      "Access-Token": "o.dYrbJOb2SzCNMBYsK0GWXiCsPXqXuryS",
+    };
+
+    axios.post(url, payload, { headers: headers });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+//Converter object to array
+//Receive old structure changes to new structure
+const converterObject = (oldStructure) => {
+  let newStructure = [];
+
+  for (const [key, value] of Object.entries(oldStructure)) {
+    //deconstruct key to date and time
+    let deconDateTime = key.split(" ");
+    let deconDate = deconDateTime[0];
+    let deconTime = deconDateTime[1];
+
+    //convert RSI string to RSI number
+    let RSIToInt = Number(value["RSI"]);
+
+    const newEntry = { date: deconDate, time: deconTime, RSI: RSIToInt };
+    if (newEntry.RSI < THRESHOLD_MIN || newEntry > THRESHOLD_MAX) {
+      newStructure.push(newEntry);
+    }
+    //console.log(newEntry);
+  }
+
+  return newStructure;
+};
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
 app.get("/rsi", async (req, res) => {
   let x = await getRSIFromAPI();
-  res.send(x);
+  let y = converterObject(x);
+  postRSIToPB(y);
+  res.send(y);
 });
 
 app.listen(port, () => {
